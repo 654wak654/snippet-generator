88a9ce0160058f519ef66c3b1baa74760cb34aa4
Ozan Eğitmen <ozanegitmen@gmail.com>
2018-11-23 15:30:46 +0300

    Remove dead code

--- extension.js
+++ extension.js
@@ -1,7 +1,6 @@
 const vscode = require("vscode");
 const path = require("path");
 const fs = require("fs");
-const os = require("os");
 const hljs = require("highlight.js");
 const mustache = require("mustache");
 const md = require("markdown-it")({
@@ -57,42 +56,9 @@ function generateSnippet() {
     let uri = document.uri;
 
     let userStyle = vscode.workspace.getConfiguration("snippet-generator").style || "atom-one-light";
-    let style = makeCss(path.join(__dirname, "styles", "markdown.css"));
+    let style = makeCss(path.join(__dirname, "markdown.css"));
     style += makeCss(path.join(__dirname, "node_modules", "highlight.js", "styles", userStyle + ".css"));
-    style += makeCss(path.join(__dirname, "styles", "markdown-pdf.css"));
-
-    let styles = vscode.workspace.getConfiguration("markdown").styles;
-    if (styles && Array.isArray(styles) && styles.length > 0) {
-        for (let i = 0; i < styles.length; i++) {
-            let hrefPath = ((resource, href) => {
-                if (!href) {
-                    return href;
-                }
-
-                const hrefUri = vscode.Uri.parse(href);
-                if (["http", "https"].indexOf(hrefUri.scheme) >= 0) {
-                    return hrefUri.toString();
-                }
-
-                if (href.indexOf("~") === 0) {
-                    return vscode.Uri.file(href.replace(/^~/, os.homedir())).toString();
-                }
-
-                if (path.isAbsolute(href) || hrefUri.scheme === "file") {
-                    return vscode.Uri.file(href).toString();
-                }
-
-                let root = vscode.workspace.getWorkspaceFolder(resource);
-                if (root) {
-                    return vscode.Uri.file(path.join(root.uri.fsPath, href)).toString();
-                }
-
-                return vscode.Uri.file(path.join(path.dirname(resource.fsPath), href)).toString();
-            })(uri, styles[i]);
-            style += "<link rel='stylesheet' href='" + hrefPath + "' type='text/css'>";
-        }
-    }
-
+ 
     // TODO: Push to git instead of generating a file on disk
     fs.writeFile(
         uri.fsPath.replace(path.extname(uri.fsPath), ".html"),
